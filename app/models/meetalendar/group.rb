6a8c8f0ce586698ae0f037556b8188080d7e143b
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-01-28 12:37:18 +0100

    more expressive use of `between?`

--- app/models/meetalendar/group.rb
+++ app/models/meetalendar/group.rb
@@ -18,12 +18,12 @@ class Meetalendar::Group < ApplicationRecord
     raise ActiveRecord::RecordNotFound("Meetup Query Location not configured!") unless Meetalendar::Frame.meetup_query_location?
 
     upcoming_events = Meetalendar::MeetupApi.find_upcoming_events(Meetalendar::Frame.meetup_query_events).select do |event|
-      event.start_time > time_now and event.start_time < time_now + 3.months and group_ids.include?(event.group_id)
+      event.start_time.between?(time_now, time_now + 3.months) and group_ids.include?(event.group_id)
     end
 
     series_events = upcoming_events.select(&:series?).map do |series|
       Meetalendar::MeetupApi.group_urlname_events(series.group_urlname, page: 5).select do |event|
-        event.start_time > time_now and event.start_time < time_now + 3.months
+        event.start_time.between?(time_now, time_now + 3.months)
       end
     end.flatten
 
